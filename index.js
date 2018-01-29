884861130603cc7e66c992767ec08443e2d9d0a0
Christian Weiss <worldmover777@gmail.com>
2018-01-29 21:40:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(katzDeliLine, name){
 
 function nowServing(katzDeliLine){
   if(katzDeliLine.length===0){
-  return "The line is currently empty"
+  return "There is nobody waiting to be served!"
   }
   else{
      var m=[]
