f9e3489eaf6aab3b006699b183508cc917e21960
Christian Weiss <worldmover777@gmail.com>
2018-01-29 21:32:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function takeANumber(katzDeliLine, name){
+  return `Welcome, ${name}. You are number ${katzDeliLine.length+1} in line.`
+}
+
+function nowServing(katzDeliLine){
+  if(katzDeliLine.length===0){
+  return "The line is currently empty"
+  }
+  else{
+    return `The line is currently: ${
+  }
+}
